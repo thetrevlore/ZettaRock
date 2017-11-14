162c7b46f8a6dd415803d59b8c032a7d535c6c8d
Trevor Storey <trevorstorey23@gmail.com>
2017-11-14 09:27:18 -0500

    [Feature], Translates text on page from english to spanish via browserAction

---
+++ app/scripts/background.js
@@ -0,0 +1,21 @@
+
+chrome.browserAction.onClicked.addListener((tab) => {
+
+  chrome.tabs.executeScript(tab.id, { file: '/scripts/scraperContentScript.js'}, _=>{
+    if(chrome.runtime.lastError !== undefined){
+      console.log(_, e);
+    }
+  })
+
+})
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(request.dictionary)
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.executeScript(tabs[0].id, { file: '/scripts/editContentScript.js'}, _=>{
+      if(chrome.runtime.lastError !== undefined){
+        console.log(_, e);
+      }
+    })
+  });
+});
